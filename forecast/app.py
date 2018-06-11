9fa7741c2fb2bfc170beb440a353f71c2deb5bd9
Rafael Lott <rafael.lott2@gmail.com>
2018-06-10 23:14:04 -0300

    Aprimorado o create_app

--- forecast/app.py
+++ forecast/app.py
@@ -2,31 +2,25 @@
 from os import path
 from flask import Flask
 from flask_restful import Api
-from forecast.api.v1.resources.user import User
-from forecast.api.v1.resources.forecast import Forecast
+from forecast.api.v1.resources import User
+from forecast.api.v1.resources import Forecast
 
 
-def start_app(mode="Development"):
+def start_api(app):
+    api = Api(app)
+    api.add_resource(User, '/api/v1/users/<user_id>')
+    api.add_resource(Forecast, '/api/v1/users/<user_id>/forecast/<forecast_id>')
+
+
+def create_app(mode="Development"):
     instance_path = path.join(
         path.abspath(path.dirname(__file__)), "{}_instance".format(mode)
     )
-
     app = Flask(
         "forecast",
         instance_path=instance_path,
         instance_relative_config=True
     )
     app.config.from_object("forecast.config.{}Config".format(mode))
-    return app
-
-
-def start_api(app):
-    api = Api(app)
-    api.add_resource(User, '/users/<user_id>')
-    api.add_resource(Forecast, '/users/<user_id>/forecast/<forecast_id>')
-
-
-def create_app():
-    app = start_app()
     start_api(app=app)
     return app
